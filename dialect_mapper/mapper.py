79ac870d3fe22bfcf65c5c77ed712c8b708bae43
Phoebe Parsons <phoebe.l.t.parsons@ntnu.no>
2022-02-15 13:40:36 +0000

    ambigious name resolution and nbtale typos

--- dialect_mapper/mapper.py
+++ dialect_mapper/mapper.py
@@ -21,6 +21,23 @@ from . import mapping_data
 # didn't want to force any other potential users to install Pandas as well
 
 class mapper_methods:
+    # ----------------- Disambiguation methods -----------------
+    def is_ambiguious_municipality(self, municipality: str) -> bool:
+        # ensure we don't have municipalities with the same name but different dialects
+        old_dialects = self.get_named_dialect_by_old_municipality(municipality)
+        new_dialects = self.get_named_dialect_by_new_municipality(municipality)
+        if old_dialects != new_dialects:
+            if old_dialects == [] or new_dialects == []:
+                return False
+            return True
+        return False
+
+    def is_ambiguious_county(self, municipality: str) -> bool:
+        # ensure we don't have municipalities with the same name but different dialects
+        if self.get_named_dialect_by_old_municipality(municipality) == self.get_named_dialect_by_new_municipality(municipality):
+            return False
+        return True
+
     # ----------------- NAMED dialect methods -----------------
     def get_old_municipalities_from_named_dialect(self, named_dialect: str) -> list:
         return sorted(list(set([x[0] for x in self.raw_csv_data if x[4].lower().strip() == named_dialect.lower().strip()])))
@@ -32,18 +49,36 @@ class mapper_methods:
         return sorted(list(set([x[3] for x in self.raw_csv_data if x[4].lower().strip() == named_dialect.lower().strip()])))
 
     def get_named_dialect_by_old_municipality(self, old_municipality) -> list:
-        return sorted(list(set([x[4] for x in self.raw_csv_data if x[0].lower().strip() == old_municipality.lower().strip()])))
+        old_municipality = old_municipality.lower().strip()
+        if self.use_nbtale_corrections:
+            old_municipality = self._get_nbtale_correction(old_municipality)
+        return sorted(list(set([x[4] for x in self.raw_csv_data if x[0].lower().strip() == old_municipality])))
     def get_named_dialect_by_new_municipality(self, new_municipality) -> list:
-        return sorted(list(set([x[4] for x in self.raw_csv_data if x[1].lower().strip() == new_municipality.lower().strip()])))
+        new_municipality = new_municipality.lower().strip()
+        if self.use_nbtale_corrections:
+            new_municipality = self._get_nbtale_correction(new_municipality)
+        return sorted(list(set([x[4] for x in self.raw_csv_data if x[1].lower().strip() == new_municipality])))
     def get_named_dialect_by_old_county(self, old_county) -> list:
         return sorted(list(set([x[4] for x in self.raw_csv_data if x[2].lower().strip() == old_county.lower().strip()])))
     def get_named_dialect_by_new_county(self, new_county) -> list:
         return sorted(list(set([x[4] for x in self.raw_csv_data if x[3].lower().strip() == new_county.lower().strip()])))
     
-    def get_named_dialect(self, lookup_by: str):
+    def get_named_dialect(self, lookup_by: str, resolve_ambigious='new'):
+        if self.is_ambiguious_municipality(lookup_by):
+            resolve_ambigious = resolve_ambigious.lower().strip()
+            if resolve_ambigious in ['new', 'old']:
+                if resolve_ambigious == 'new':
+                    dialects = self.get_named_dialect_by_new_municipality(lookup_by)
+                    return dialects[0] if len(dialects) == 1 else dialects
+                else:
+                    dialects = self.get_named_dialect_by_old_municipality(lookup_by)
+                    return dialects[0] if len(dialects) == 1 else dialects
+            else:
+                print("Unknown way of resolving ambigious municipality for {}. Using new municipality.".format(lookup_by))
+
         # by looking up by old municipality first we're prioritizing it. I don't have a super strong arguement as to the why,
         # presumably old municipalities will have fewer one to many mappings. But, if we feel like going with the new municipalities
-        # is better this can easily be changed
+        # is better this can easily be changed                
         dialects = self.get_named_dialect_by_old_municipality(lookup_by)
         if len(dialects) > 0:
             return dialects[0] if len(dialects) == 1 else dialects
@@ -74,15 +109,33 @@ class mapper_methods:
         return sorted(list(set([x[3] for x in self.raw_csv_data if int(x[5]) == int(named_dialect)])))
 
     def get_numeric_dialect_by_old_municipality(self, old_municipality) -> list:
-        return sorted(list(set([x[5] for x in self.raw_csv_data if x[0].lower().strip() == old_municipality.lower().strip()])))
+        old_municipality = old_municipality.lower().strip()
+        if self.use_nbtale_corrections:
+            old_municipality = self._get_nbtale_correction(old_municipality)
+        return sorted(list(set([x[5] for x in self.raw_csv_data if x[0].lower().strip() == old_municipality])))
     def get_numeric_dialect_by_new_municipality(self, new_municipality) -> list:
-        return sorted(list(set([x[5] for x in self.raw_csv_data if x[1].lower().strip() == new_municipality.lower().strip()])))
+        new_municipality = new_municipality.lower().strip()
+        if self.use_nbtale_corrections:
+            new_municipality = self._get_nbtale_correction(new_municipality)
+        return sorted(list(set([x[5] for x in self.raw_csv_data if x[1].lower().strip() == new_municipality])))
     def get_numeric_dialect_by_old_county(self, old_county) -> list:
         return sorted(list(set([x[5] for x in self.raw_csv_data if x[2].lower().strip() == old_county.lower().strip()])))
     def get_numeric_dialect_by_new_county(self, new_county) -> list:
         return sorted(list(set([x[5] for x in self.raw_csv_data if x[3].lower().strip() == new_county.lower().strip()])))
     
-    def get_numeric_dialect(self, lookup_by: str):
+    def get_numeric_dialect(self, lookup_by: str, resolve_ambigious='new'):
+        if self.is_ambiguious_municipality(lookup_by):
+            resolve_ambigious = resolve_ambigious.lower().strip()
+            if resolve_ambigious in ['new', 'old']:
+                if resolve_ambigious == 'new':
+                    dialects = self.get_numeric_dialect_by_new_municipality(lookup_by)
+                    return dialects[0] if len(dialects) == 1 else dialects
+                else:
+                    dialects = self.get_numeric_dialect_by_old_municipality(lookup_by)
+                    return dialects[0] if len(dialects) == 1 else dialects
+            else:
+                print("Unknown way of resolving ambigious municipality for {}. Using new municipality.".format(lookup_by))
+
         # by looking up by old municipality first we're prioritizing it. I don't have a super strong arguement as to the why,
         # presumably old municipalities will have fewer one to many mappings. But, if we feel like going with the new municipalities
         # is better this can easily be changed
@@ -105,12 +158,32 @@ class mapper_methods:
                         print("ERROR: cannot find numeric dialect for: {}".format(lookup_by))
                         return None
 
+    def enable_nbtale_corrections(self) -> None:
+        # NBTale has some human errors in the kommune names. I've created a mapping from the NB Tale names to what they should be
+        # this method will switch the flag so later queries use the corrected mapping and load the mapping data
+        # NOTE: this will only correct the input. Presumably the resource table has the correct names
+        self.use_nbtale_corrections = True
+        cReader = csv.reader(
+            StringIO(
+                pkg_resources.read_text(
+                    mapping_data, 
+                    'nbtale_transform.csv'
+                )
+            )
+        )
+        for row in cReader:
+            self.nbtale_corrections[row[0]] = row[1]
+
+    def _get_nbtale_correction(self, lookup_by: str) -> str:
+        if lookup_by in self.nbtale_corrections:
+            lookup_by = self.nbtale_corrections[lookup_by]
+        return lookup_by
+
     def __init__(self) -> None:
+        self.use_nbtale_corrections = False
         self.raw_csv_data = []
-        # with open(
-        #     pkg_resources.read_text(mapping_data, 'muni_county_namedDialect_numericDialect_mapping_manual_additions_renamed.csv'), # './dialect_mapper/muni_county_namedDialect_numericDialect_mapping_manual_additions_renamed.csv',
-        #     'r'
-        # ) as f_open:
+        self.nbtale_corrections = {}
+
         cReader = csv.reader(
             StringIO(
                 pkg_resources.read_text(
